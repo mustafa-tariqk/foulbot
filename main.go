79e7d224e4d683a8e3f61512c4a5d79dca18c348
mustafa-tariqk <mustafa.tariqk@icloud.com>
2025-01-24 21:40:44 -0800

    getting rid of made by string

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/inconshreveable/go-update"
 )
-
+a
 var (
 	VERSION     string
 	CONFIG_JSON = "config.json"
@@ -542,7 +542,7 @@ func create_leaderboard(year string, userId string) *discordgo.MessageEmbed {
 		description += fmt.Sprintf("%s <@%s>: %d\n", NUMBERS[i], position.UserId, position.Points)
 	}
 	return &discordgo.MessageEmbed{
-		Title:       fmt.Sprintf("Leaderboard (made by %s)", userId),
+		Title:       fmt.Sprintf("Leaderboard (%s)", userId),
 		Description: description,
 	}
 }
