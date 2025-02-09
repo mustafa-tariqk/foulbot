4c428a5226f12a236aea2e1feee334dea2f235b0
mustafa-tariqk <mustafa.tariqk@icloud.com>
2025-02-09 13:02:30 -0800

    Creator shown on poll creation

--- inputs/inputs.go
+++ inputs/inputs.go
@@ -123,6 +123,11 @@ func HandleInputs(bot *discordgo.Session) {
 							Title: "Own",
 							Fields: []*discordgo.MessageEmbedField{
 								{
+									Name:   "Creator",
+									Value:  fmt.Sprintf("<@%s>", i.Member.User.ID),
+									Inline: true,
+								},
+								{
 									Name:   "Gainers",
 									Value:  formatUserMentions(users),
 									Inline: true,
